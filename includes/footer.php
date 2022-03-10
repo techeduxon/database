4dc371722119157f8952bd6061f049172aea1edb
fazil <fazilmark1@gmail.com>
2022-03-10 16:21:26 +0530

    homepage code

---
+++ includes/footer.php
@@ -0,0 +1,70 @@
+  <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+
+
+          <div class="col-lg-3 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+            <div class="col-lg-3 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a</li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+               <div class="col-lg-3 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a</li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by 
+         <a href="#">GeoPITS</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" data-toggle="tooltip" data-placement="top" title="Facebook"  href="#"><i class="fab fa-facebook-f"></i></a></li>
+              <li><a class="twitter" data-toggle="tooltip" data-placement="top" title="twitter"  href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a class="dribbble" data-toggle="tooltip" data-placement="top" title="dribbble"  href="#"><i class="fab fa-dribbble"></i></a></li>
+              <li><a class="linkedin" data-toggle="tooltip" data-placement="top" title="linkedin"  href="#"><i class="fab fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,257 @@
+
+<?php 
+include ('includes/header.php');
+?>
+<div class="container">
+
+   
+   <div class="row pt-5 one">
+    <div class="col-lg-7 col-md-7 col-sm-6">
+    <h6 class="sty1"> Geo Platinum IT services</h6>
+    <h1 class="mb-3" >Your trusted data <br> management experts</h1>
+    <p>
+    Consulting, Support & Managed Services for MS SQL, at the most affordable cost, tailor made for your requirement.
+    </p><br>
+    <button type="button" class="btn btn-primary">Get Quote </button> &nbsp;(or)<a href="#" class="sty2 ml-3">Schedule free consultation</a>
+    
+    <p>Recognitions</p>
+
+    <div class="row iso">
+        <div class="col-sm-4"><img   class="img-fluid lg" src="./assets/images/iso.png" alt="banner"  loading="lazy"></div>
+        <div class="col-sm-4"><img   class="img-fluid lg" src="./assets/images/iso.png" alt="banner"  loading="lazy"></div>
+        <div class="col-sm-4"><img   class="img-fluid lg" src="./assets/images/iso.png" alt="banner"  loading="lazy"></div>
+      </div>
+     </div>
+     <div class="col-lg-5 col-md-5 col-sm-6">
+         <div class="banner"><img   class="img-fluid banner " src="./assets/images/banner.jpeg" alt="banner"  loading="lazy">
+        </div>
+       </div>
+  </div>
+ <br>
+ <br>
+
+  <div class="section-title-wrap text-center">
+          <h3>Reliable Data & Managed Services Partner For <span class="com">45+ <br> Companies</span></h3>
+<marquee behavior="scroll" direction="left" scrollmount = "#" >
+        <ul class="logos">
+          <li><img src="./assets/images/logo1.jpeg" alt="companies-logo"></li>
+          <li><img src="./assets/images/logo1.jpeg" alt="companies-logo"></li>
+          <li><img src="./assets/images/logo1.jpeg" alt="companies-logo"></li>
+          <li><img src="./assets/images/logo1.jpeg" alt="companies-logo"></li>
+          <li><img src="./assets/images/logo1.jpeg" alt="companies-logo"></li>
+       </ul>
+       </marquee>
+  </div>
+  <hr>
+  <div class="bg p-2" >
+   <div class="row">
+     <div class="col-lg-12  text-center pro ">
+      <h6 >OUR SERVICES</h6>
+      <p>GeoPITS provides expert services in database management, analytics operations, and <br>
+         application solutions. Professional services designed to suit your needs!</p>
+   </div>
+     <div class="row two ">
+           <div class="  col-md-3 col-sm-4 card ">
+          <img src="./assets/images/support.gif" alt="companies-logo" width="110px">
+          <h4>Remote DBA Support</h4> 
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+          enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          </div>
+          <div class=" col-md-3 col-sm-4 card">
+         <img src="./assets/images/cloud.gif" alt="companies-logo" width="110px">
+         <h4>Cloud Database <br> Management</h4>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+         </div>
+         <div class=" col-md-3 col-sm-4 card">
+         <img src="./assets/images/analytics.gif" alt="companies-logo" width="110px">
+         <h4>Analytics & BI</h4>       
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+         </div>
+       </div>
+         <div class="row two performance"> 
+           <div class="col-sm-4 col-md-3 card ">
+           <img src="./assets/images/opti.gif" alt="companies-logo" width="110px">
+           <h4>Performance Optimization</h4>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+         <div class="col-sm-4 col-md-3 card">
+            <img src="./assets/images/data.gif" alt="companies-logo" width="110px">
+            <h4>Database Migration Migration</h4>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+           </div>
+           <div class="col-sm-4 col-md-3 card ">
+           <img src="./assets/images/enter.gif" alt="companies-logo" width="110px">
+           <h4>Enterprise Application Development</h4>        
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+         </div>
+     </div>
+   </div>
+  
+  </div>
+  <div class="container mt-5 bg-dark">
+   <div class="row status">
+    <div class="col-sm-3">
+      <h4>45+</h4>
+     <p>HAPPY CLIENTS</p>      
+    </div>
+    <div class="col-sm-3">
+      <h4>750+</h4><P>DATABASE BEING <BR> MANAGED</BR></P>
+    
+    </div>
+    <div class="col-sm-3">
+      <h4>75+</h4> <p>DATABASE MIGRATIONS TO <BR>CLOUD</BR></p>       
+     
+    </div>
+    <div class="col-sm-3">
+      <h4>2M+</h4><P>DB TRANSACTIONS <BR>HANDLED PER DAY</BR></P>        
+     
+    </div>
+   </div>
+</div>
+<section class="projects">
+  <div class="content text-center ">
+  <h2>Projects that we are proud of and that make us stand out</h2>
+  <p>See for yourself how our customers have used our solutions to successfully handle their database problems.</p>
+  </div>
+
+  <div class="row pt-5">
+    <div class="col-lg-6">
+      <h4>With our managed services, a collaborative SaaS product firm creates a stable and scalable SQL environment.</h4>
+      <br>
+      <p>Explore how the organisation was able to boost database uptime and extend the infrastructure to serve 1M+ users across diverse industries with round-the-clock database support from our DBA professionals.</p>
+      <br>
+      <button type="button" class="btn  bot">View case Study</button>
+    </div>
+    <div class="col-lg-6">
+    <img class="img-fluid img2 " src="./assets/images/imgg.gif" alt="banner"  loading="lazy">
+    <br><br>
+    </div> 
+    <hr>
+   
+    <div class="col-lg-5 ">
+    <img class="img-fluid img3 " src="./assets/images/img3.png" alt="banner"  loading="lazy">
+    </div>
+    <div class="col-lg-6 p-5">
+      <div class="con">
+      <br><br>
+    <h4>The Business Critical Reporting Process has been tripled!</h4> 
+    <span class="line">GeoPITS India</span>
+      <br>
+      <p>Learn how GeoPITS assisted one of India's largest multinational motorcycle firms in improving database performance, consolidating, and optimising their SQL Server settings.</p>
+      </div>
+      <br>
+      <button type="button" class="btn  bot ">View case Study</button>
+    </div>
+  </div>
+
+</section>
+<hr>
+  <!-- demo -->
+  <!-- <section class="third" > 
+    <h5 >OUR RECOGNITIONS</h5>
+    <div class="row ">
+      <div class="col ">YOURSTORY</div>
+      <div class="col">MY STORY</div>
+      <div class="col">OWN STORY</div>
+    </div>
+  </section> -->
+<!-- dasdas    -->
+<!-- Testimonial Slider Section -->
+<section id="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-title">
+                       <h5 class="test">TESTIMONIALS</h5>
+                        <h2 class="">Why do people prefer to work with GeoPITS?</h2>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div id="testimonialCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Slide Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#testimonialCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#testimonialCarousel" data-slide-to="1"></li>
+                            <li data-target="#testimonialCarousel" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <!-- Slide 1 -->
+                            <div class="carousel-item active">
+                                <div class="carousel-content">
+                                    <div class="client-img "><img src="./assets/images/e1.png" alt="Testimonial Slider" /></div>
+                                    <p><i>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</i></p>
+                                    <h3><span>-</span> Shaik Fazil Ahmed <span>-</span></h3>
+                                </div>
+                            </div>
+                            <!-- Slide 2 -->
+                            <div class="carousel-item">
+                                <div class="carousel-content">
+                                    <div class="client-img"><img src="./assets/images/e2.png" alt="Testimonial Slider" /></div>
+                                    <p><i>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. There are many variations of passages of Lorem Ipsum available. Lorem Ipsum is simply dummy text of the printing and typesetting industry</i></p>
+                                    <h3><span>-</span> Shankarnath <span>-</span></h3>
+                                </div>
+                            </div>
+                            <!-- Slide 3 -->
+                            <div class="carousel-item">
+                                <div class="carousel-content">
+                                    <div class="client-img"><img src="./assets/images/img3.png" alt="Testimonial Slider" /></div>
+                                    <p><i>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form. There are many variations of passages of Lorem Ipsum available. Lorem Ipsum is simply dummy text of the printing and typesetting industry</i></p>
+                                    <h3><span>-</span> Sri latha<span>-</span></h3>
+                                </div>
+                            </div>
+                            <!-- Slider pre and next arrow -->
+                            <a class="carousel-control-prev text-success" href="#testimonialCarousel" role="button" data-slide="prev">
+                                <i class="fas fa-chevron-left"></i>
+                            </a>
+                            <a class="carousel-control-next text-success" href="#testimonialCarousel" role="button" data-slide="next">
+                                <i class="fas fa-chevron-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Testimonial Slider Section -->
+    
+    <section class="third" > 
+    <h5 >OUR RECOGNITIONS</h5>
+    <div class="row ">
+      <div class="col ">YOURSTORY</div>
+      <div class="col">MY STORY</div>
+      <div class="col">OWN STORY</div>
+    </div>
+  </section>
+<hr>
+    <section class="four">
+      <div class="row">
+        <div class="col-md-5 ">
+          <div class="text">
+            <h4>Let's talk about business for a moment. Give us a call</h4>
+            <p>Trust us to make data management simple and effective. We will make every effort to turn your experience into the finest comment ever.</p>
+          </div>
+        </div>
+        <div class="col-md-5 text-center">
+
+        <!-- <img src="./assets/images/phone.gif" alt="companies-logo" width="110px"> -->
+        <div class="text1">
+        <img src="./assets/images/phone.gif" alt="companies-logo" width="110px">
+        <h5 >REACH OUT NOW</h5>
+        <H1>+91 97845 61235</H1>
+        <button type="button" class="btn btn-primary">Contact us</button>
+        </div>
+        
+        </div>
+      </div>
+    </section>
+</div>
+<?php 
+include ('includes/footer.php');
+?>
+</body>
+</html>
